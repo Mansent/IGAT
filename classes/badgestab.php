b75ae1febec9c9b84118ac68bede4d665f628925
Manuel Gottschlich <manuel.gottschlich@rwth-aachen.de>
2019-08-02 13:23:17 +0200

    Encapsule badge rendering

--- classes/badgestab.php
+++ classes/badgestab.php
@@ -13,17 +13,25 @@ class block_igat_badgestab {
    * @param courseid the id of the current course.
    */
   public function render_tab($courseid) {
-	//$output = $PAGE->get_renderer('core', 'badges');
-    $records = badges_get_badges(2, $courseid);
-	//echo '<pre>' . var_export($records, true) . '</pre>';
+	global $USER;
 	
-    $badges = json_decode($records, true);
+	$records = badges_get_badges(2, $courseid, '', '', 0, 1000, $USER->id);
+	
+	echo '<h2>Your Badges</h2>';
 	foreach($records as &$badge) {
-		block_igat_badgestab::renderBadge($badge);
+		if($badge->dateissued != null) { // user owns badge
+			block_igat_badgestab::renderBadge($badge);
+		}
 	}
-   
-    //$badges = new \core_badges\output\badge_collection($records);
-    //echo $output->render($badges);
+	
+	echo '<h2>Available Badges</h2>';
+	echo '<div class="igatbadgescontainer">';
+	foreach($records as &$badge) {
+		if($badge->dateissued == null) { // user has not yet achieved badge
+			block_igat_badgestab::renderBadge($badge);
+		}
+	}
+	echo '</div>';
   }
   
   private function renderBadge($badge) {
@@ -31,16 +39,39 @@ class block_igat_badgestab {
 	$id = $badge->id;
 	$name = $badge->name;
 	$description = $badge->description;
+	$uniquehash = $badge->uniquehash;
+	$userOwnsBadge = $badge->dateissued != null;
 	$criteria = $badge->criteria;
 	$completioncriteria = $criteria[1];
 	$criteriadescription = $completioncriteria->description;
-		
-	echo '<div class="igatbadge">';
+	
+	// url to badge page only available if user owns badge
+	$badgeUrl = null;
+	if($userOwnsBadge) {
+		$badgeUrl = "/badges/badge.php?hash=" . $uniquehash;
+	}
+	
+	//determine background color
+	$colorClass = null;
+	if($userOwnsBadge) {
+		$colorClass = "igatbadgeowned";
+	}
+	else {
+		$colorClass = "igatbadgeavailable";
+	}
+	
+	if($userOwnsBadge) echo '<a href="' . $badgeUrl . '">';
+	echo '<div class="igatbadge ' . $colorClass . '">';
 	echo print_badge_image($badge, $PAGE->context, "f3");
 	echo "<h3>$id: $name</h3>";
-	echo "<p>$description<p>";
-	echo "<p>$criteriadescription</p>";
+	if ($userOwnsBadge) {
+		echo "<p>$description<p>";
+	}
+	else {
+		echo "<p>$criteriadescription</p>";
+	}
 	echo '</div>';
+	if ($userOwnsBadge) echo '</a>';
   }
 }
 ?>
