c2bb93a6055fc3c22a65fd3f88fef83483076f63
Manuel Gottschlich <manuel.gottschlich@rwth-aachen.de>
2019-08-27 16:45:08 +0200

    Add logging library#

--- ajax.php
+++ ajax.php
@@ -3,29 +3,20 @@
  * This file provides the server backennd for ajax requests from the igat plugin.
  */ 
 require_once('../../config.php');
+require_once('classes/lib/igat_logging.php');
 
 global $DB, $OUTPUT, $PAGE, $USER;
-if(!empty($_POST['loadtime']) && !empty($_POST['url']) && !empty($_POST['leavetime']) 
-		&& !empty($_POST['destination']) && !empty($_POST['courseid'])) {
-	$loadtime = $_POST['loadtime'];
-	$url = $_POST['url'];
-	$leavetime = $_POST['leavetime'];
-	$destination = $_POST['destination'];
+if(!empty($_POST['courseid']) && !empty($_POST['loadtime']) && !empty($_POST['url']) 
+		&& !empty($_POST['leavetime']) && !empty($_POST['destination'])) {
+			
 	$courseId = $_POST['courseid'];
-	
-	$tab = substr($url, strrpos($url, '=') + 1);
-	if(!in_array($tab, ['progress', 'badges', 'ranks', 'settings'], true)) {
-		die('invalid parameter');
-	}
-	$duration = $leavetime - $loadtime;
-	$nextPage = parse_url($destination, PHP_URL_PATH) . '?' . parse_url($destination, PHP_URL_QUERY);
-	
-	$DB->insert_record('block_igat_dashboard_log', array('courseid' => $courseId,
-																											 'userid' => $USER->id,
-																											 'time' => $loadtime, 
-																											 'duration' => $duration,
-																											 'tab' => $tab,  
-																											 'next_page' => $nextPage));
+	$loadtime = $_POST['loadtime'];
+	$leavetime = $_POST['leavetime'];
+	$url = $_POST['url'];
+	$destination = $_POST['destination'];
+
+	$lib_logging = new igat_logging();
+	$lib_logging->logDashboardVisit($courseId, $USER->id, $loadtime, $leavetime, $url, $destination);
 }
 
 ?>
---
+++ classes/lib/igat_logging.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Library for logging user behavior data and managing logs.
+ */
+class igat_logging
+{
+	/**
+	 * Writes a visit of the gamification dashboard to the logfiles
+	 * @param int $courseId the id of the course the user visited
+	 * @param int $userId the id of the user visited the page
+	 * @param int $loadtime the unix time the user has loaded the page
+	 * @param int $leavetime the unix time the user left the page
+	 * @param string $url the url of the page the user visited
+	 * @param string $destination the url the user opened after leaving this page or null if unknown
+	 */
+  public function logDashboardVisit($courseId, $userId, $loadtime, $leavetime, $url, $destination) {
+		global $DB;
+		
+		// parse tab
+		$tab = substr($url, strrpos($url, '=') + 1);
+		if(!in_array($tab, ['progress', 'badges', 'ranks', 'settings'], true)) {
+			return; // this should never happen
+		}
+		
+		// calculate duration
+		$duration = $leavetime - $loadtime;
+		
+		// parse next page
+		$nextPageURL = parse_url($destination, PHP_URL_PATH);
+		if($nextPageURL == "/blocks/igat/dashboard.php") { // another tab was opened
+			$nextPageParams = parse_url($destination, PHP_URL_QUERY);
+			$newTab =  substr($destination, strrpos($destination, '=') + 1);
+			
+			if(in_array($newTab, ['progress', 'badges', 'ranks', 'settings'], true)) {
+				$nextPage = $newTab;
+			}
+			else {
+				$nextPage = 'external';
+			}
+		}
+		else if($destination != 'undefined') { // user left moodle or closed the browser
+			$nextPage = 'moodle';
+		}
+		else { 
+			$nextPage = 'external';
+		}
+		
+		// write log to db
+		$DB->insert_record('block_igat_dashboard_log', array('courseid' => $courseId,
+																												 'userid' => $userId,
+																												 'time' => $loadtime, 
+																												 'duration' => $duration,
+																												 'tab' => $tab,  
+																												 'next_page' => $nextPage));
+	}
+}
